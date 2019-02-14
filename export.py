1795efe65c9c9ec8fd11fc6df81d0955c2e86c46
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-02-14 18:32:05 +0900

    revise

---
+++ export.py
@@ -0,0 +1,28 @@
+# encoding: utf-8
+import numpy as np
+import chainer
+import chainer.links as L
+import onnx_chainer
+import onnx
+#import cupy
+from yolov2 import YOLOv2
+
+#model = L.VGG16Layers()
+model = YOLOv2(80,5)
+
+# ネットワークに流し込む擬似的なデータを用意する
+x = np.zeros((1, 3, 416, 416), dtype=np.float32)
+
+# 推論モードにする
+chainer.config.train = False
+
+print("save as onnx model")
+with chainer.using_config('train',False):
+    onnx_model = onnx_chainer.export(model, x, filename='YOLOv2.onnx', save_text=True)
+    print("load onnx model")
+    modelx = onnx.load("YOLOv2.onnx")
+    print("nodes",len(modelx.graph.node))
+    #for i , node in enumerate(modelx.graph.node):
+    #    print("[Node #{}]".format(i))
+    #    print(node)
+    #    if i==15: break
