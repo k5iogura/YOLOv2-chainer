92f1a9d50b99417508543d26c6de4010de1277e3
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-02-15 18:58:20 +0900

    revise

--- npz2npz4custom_load.py
+++ npz2npz4custom_load.py
@@ -6,21 +6,21 @@ import numpy as np
 import chainer
 import chainer.links as L
 from chainer import serializers
-from yolov2_orig import YOLOv2
+from yolov2 import YOLOv2, load_npz
 
-parser = argparse.ArgumentParser(description="hdf5 to npz transform")
-parser.add_argument('hdf5', help="input hdf5 format")
-parser.add_argument('npz',  help="input npz format")
+parser = argparse.ArgumentParser(description="npz1 to npz transform")
+parser.add_argument('npz1', help="input npz format")
+parser.add_argument('npz2', help="output npz format")
 args = parser.parse_args()
 
 print("define model (80,5)")
 model = YOLOv2(80,5)
 
-if os.path.exists(args.hdf5):
-    print("load hdf5 model",args.hdf5)
-    serializers.load_hdf5(args.hdf5, model)
+if os.path.exists(args.npz1):
+    print("custom load npz1 model",args.npz1)
+    load_npz(args.npz1, model)
 else:
-    print(args.hdf5,"not found")
+    print(args.npz1,"not found")
     sys.exit(-1)
 
 x = np.zeros((1, 3, 416, 416), dtype=np.float32)
@@ -31,5 +31,5 @@ with chainer.using_config('train',False):
     print("infer for dummy data", x.shape)
     result = model(x)
     print("result",result.shape, type(result))
-    print("save as npz model", args.npz)
-    serializers.save_npz(args.npz, model)
+    print("save as npz model", args.npz2)
+    serializers.save_npz(args.npz2, model)
